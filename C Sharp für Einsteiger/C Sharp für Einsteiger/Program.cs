143effb63cd17e4caaed45511322a108038b3080
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-27 20:14:23 +0200

    aufgaben zu schleifen

--- C Sharp für Einsteiger/C Sharp für Einsteiger/Program.cs
+++ C Sharp für Einsteiger/C Sharp für Einsteiger/Program.cs
@@ -10,6 +10,45 @@ namespace C_Sharp_für_Einsteiger
     {
         static void Main(string[] args)
         {
+            //S.106 Gebe die Quadratzahlen von 1-10 in einer while, do while und for schleife aus
+            //int zahl = 1;
+            //while (zahl <= 10)
+            //{
+            //    Console.WriteLine(zahl*zahl);
+            //    zahl++;
+            //}
+            //for (zahl = 1; zahl <= 10; zahl++) 
+            //{
+            //    Console.WriteLine(zahl*zahl);
+            //}
+            //zahl = 1;
+            //do
+            //{
+            //    Console.WriteLine(zahl*zahl);
+            //    zahl++;
+            //} while (zahl <= 10);
+            //Console.ReadKey();
+
+            //S.106 Namen abfragen und wenn benutzer fertig in liste speichern und in foreach schleife ausgeben
+            //char weiter;
+            //string name;
+            //List<string> namenliste = new List<string>();
+            //do
+            //{
+            //    Console.WriteLine("Bitte gebe mir einen Namen");
+            //    name = Console.ReadLine();
+            //    namenliste.Add(name);
+
+            //    Console.WriteLine("Willst du einen weiteren namen hinzufügen? j/n");
+            //    weiter = char.Parse(Console.ReadLine());
+            //}while (weiter == 'j');
+            //foreach (string inhalt in namenliste)
+            //{
+            //    Console.WriteLine(inhalt);
+            //}
+            //Console.ReadKey();
+
+
         }
     }
 }
